eda1c13ed1b6fccb79ad898e8deb1a9d4488eeae
tanaka <lemo.nade.engineer.room@gmail.com>
2022-06-24 03:44:16 +0900

    FileServerを使わずにしてみる

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,9 +7,34 @@ import { GameController } from "./controllers/gameController.ts"
 import { MemoryRoomRecordRepository } from "./repositories/inmemory/memoryRoomRecordRepository.ts"
 
 const routes = (app: L.Application): void => {
-    app.grouped('**')
-        .grouped(new L.FileMiddleware(app.directory.publicDirectory))
-        .get((_req) => new Promise(_resolve => {}))
+
+    const fileResponse = async (filename: string, type: string) => new Response(await Deno.readTextFile(app.directory.publicDirectory + filename), { headers: {"Content-Type": `text/${type}; charset=utf-8`}})
+
+
+    app.get('index.html', async () => {
+        const res = await fileResponse('index.html', 'html')
+        return { type: 'responsible', response: () =>  res}
+    })
+
+    app.get('favicon.ico', async () => {
+        const file = await Deno.readFile(app.directory.publicDirectory + 'favicon.ico')
+        return { type: 'responsible', response: () =>  new Response(file, { headers: {"Content-Type": "image/x-icon"}})}
+    })
+
+    app.get(['assets', 'index.e1a413ba.css'], async () => {
+        const res = await fileResponse('assets/index.e1a413ba.css', 'css')
+        return { type: 'responsible', response: () =>  res}
+    })
+
+    app.get(['assets', 'index.fcbde9b8.js'], async () => {
+        const res = await fileResponse('assets/index.fcbde9b8.js', 'javascript')
+        return { type: 'responsible', response: () =>  res}
+    })
+
+    app.get('**', async () => {
+        const res = await fileResponse('index.html', 'html')
+        return { type: 'responsible', response: () =>  res}
+    })
 
     const api = app.grouped('api').grouped(new LogMiddleware())
     api.register(new LoginController())
